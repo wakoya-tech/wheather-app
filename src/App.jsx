5366c8de2091036f3121028db6e97578eb9938e3
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-11-02 14:36:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
 import Weather from './Components/Weather'
 import './App.css'
-
 function App() {
- 
  return(
  <div className='App'>
 <Weather/>
-
  </div>
-
  )
-
 }
 
 export default App
